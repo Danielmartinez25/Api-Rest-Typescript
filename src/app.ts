2498d12b1e687427e83bae9c0babb4906c68c9c4
Danimartinez25 <danimartinez1325@gmail.com>
2023-06-05 23:40:11 -0300

    create routes and controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-const PORT = process.env.PORT || 3001
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+const PORT = process.env.PORT || 3001;
+import {router} from './routes';
+import db from './config/mongo'
 const app = express()
 app.use(cors())
+app.use(router)
+db().then(()=> console.log('Conexion exitosa'));
+app.listen(PORT,()=> console.log(`port using http://localhost${PORT}`));
+
---
+++ src/controllers/item.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express"
+import { handleHttp } from "../utils/error.handle"
+
+const getItem = (req: Request, res: Response) => {
+    try {
+
+    } catch (e) {
+        handleHttp(res, 'ERROR_GET_ITEM')
+    }
+}
+const getItems = (req: Request, res: Response) => {
+    try {
+
+    } catch (e) {
+        handleHttp(res, 'ERROR_GETS_ITEM')
+
+    }
+}
+const updateItem = (req: Request, res: Response) => {
+    try {
+
+    } catch (e) {
+        handleHttp(res, 'ERROR_UPDATE_ITEM')
+
+    }
+}
+const postItem = (req: Request, res: Response) => {
+    try {
+
+    } catch (e) {
+        handleHttp(res, 'ERROR_POST_ITEM')
+
+    }
+}
+const deleteItem = (req: Request, res: Response) => {
+    try {
+
+    } catch (e) {
+        handleHttp(res, 'ERROR_DELETE_ITEM')
+
+    }
+}
+export { getItem, getItems, updateItem, deleteItem, postItem };
